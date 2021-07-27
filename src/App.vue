b33c94e5069ea0a687dd14a4d4326fd687153468
warren yun <xxwarrenhdxx@gmail.com>
2021-07-26 23:09:42 -0400

    Restyle app with vue router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+<template>  
   <router-view/>
 </template>
 
 <style>
+:root {
+  --primary-color: #42b983;
+  --primrary-color-dark: #189e74;
+  --text-color: #2c3e50;
+  --background-color: #fff;
+}
+[data-theme="darkMode"] {
+  --text-color: #fff;
+  --background-color: #242629;
+}
+* {
+  background-color: var(--background-color);
+}
+
+body {
+  overflow: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--text-color);
 }
+
 
 #nav {
   padding: 30px;
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="home">
+  <ToggleTheme />
+  <div class="startContainer">
     <Title />
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
 import Title from '@/components/Title.vue';
+import ToggleTheme from '@/components/ToggleTheme.vue';
 
 export default {
   name: 'Start',
   components: {
-    Title
+    Title, ToggleTheme
   }
 }
 </script>
+
+<style>
+  .startContainer {
+    height: 95vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+</style>
